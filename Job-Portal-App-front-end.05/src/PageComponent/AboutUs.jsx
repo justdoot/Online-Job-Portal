b9f19ecf3c61c81ef020134bb4c433a4612fa83e
Shivendra Pratap Singh <shivendrapratap18@gmail.com>
2024-02-18 21:06:35 +0530

    changes in layout ,routing almost done

---
+++ Job-Portal-App-front-end.05/src/PageComponent/AboutUs.jsx
@@ -0,0 +1,41 @@
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <h4 className="mb-3">About Us</h4>
+      <b>
+        At our online job portal, our mission is to connect job seekers with the right
+        opportunities and help organizations find the best candidates for their job openings.
+        We understand the importance of finding the perfect match, and we strive to make the job
+        search and hiring process efficient and effective.
+        <br />
+        <br />
+        <h6 className="mb-4">Features and Benefits:</h6>
+        Extensive Job Listings: Our platform offers a wide range of job listings from various industries, allowing job seekers to explore diverse opportunities.
+        <br />
+        User-Friendly Interface: We provide an easy-to-use interface that allows both job seekers and employers to navigate the platform effortlessly.
+        <br />
+        Advanced Search Functionality: Job seekers can use our advanced search filters to find relevant job openings based on their preferences, such as location, industry, experience level, and more.
+        <br />
+        Resume Parsing: Our platform utilizes resume parsing technology to extract relevant information from job seekers' resumes, making it easier for employers to find the best match for their job openings.
+        <br />
+        Candidate Screening: We offer tools and features that help employers screen and evaluate candidates, saving time and ensuring they find the most qualified individuals.
+        <br />
+        Job Alerts: Job seekers can set up personalized job alerts to receive notifications about new job openings that match their criteria, ensuring they never miss out on potential opportunities.
+        <br />
+        Company Profiles: Employers can create detailed company profiles to showcase their organization's culture, values, and benefits, helping them attract top talent.
+        <br />
+        Career Resources: We provide valuable career resources, including interview tips, resume writing guidance, and career advice articles, to support job seekers in their professional development.
+        <br />
+        <br />
+        <h6 className="mb-4">Who Can Use Our Online Job Portal?</h6>
+        Our platform is designed to serve both job seekers and employers. Job seekers can use our portal to search for
+         job opportunities, apply for positions, and enhance their career prospects. Employers can utilize our platform to post 
+         job openings, search for qualified candidates, and streamline their hiring process.
+         <br />
+         We aim to create a platform that brings together job seekers and employers, facilitating meaningful connections and fostering successful careers. Join us at our online job portal and let us help you find the perfect match for your career aspirations or job openings.
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ Job-Portal-App-front-end.05/src/PageComponent/ContactUs.jsx
@@ -0,0 +1,23 @@
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <h4 className="mb-3">Contact Us</h4>
+      <b>
+        We value your feedback, questions, and inquiries. Whether you need
+        assistance with an order, have a suggestion, or simply want to connect,
+        our team is here to help.
+        <h6 className="mb-3">Phone:</h6>
+        You can contact our customer support team by phone at [8527417762]. <br />
+        Our representatives are available [10:00AM-5:00PM] to assist you with any inquiries or issues you may have.
+        <h6 className="mb-3">Email:</h6>
+        For any non-urgent inquiries or general questions, you can reach us via email at [shivendrapratap18@gmail.com].<br />
+         We strive to respond to all emails within [24 hours] during our operating hours.
+        <h6 className="mb-3">Online Support:</h6>
+        We also provide online support through our website. You can visit our Help Center or Support page to find answers to 
+        frequently asked questions and access resources that may address your concerns.
+      </b>
+    </div>
+  );
+};
+
+export default ContactUs;
